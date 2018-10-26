19ba9bfb11f812326175bffdc49962cf4cef08d0
Yevseyenko <jevsejen@gmail.com>
2018-10-27 01:24:56 +0300

    Added some features

--- src/main/java/com/epam/restclient/RESTClient.java
+++ src/main/java/com/epam/restclient/RESTClient.java
@@ -1,72 +1,25 @@
 package com.epam.restclient;
 
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
-
 import com.sun.jersey.api.client.Client;
-import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
+import com.sun.jersey.api.client.config.ClientConfig;
+import com.sun.jersey.api.client.config.DefaultClientConfig;
 import org.apache.log4j.Logger;
 
-
 import javax.ws.rs.core.MediaType;
-import javax.xml.ws.Response;
-import java.io.IOException;
-
-import java.io.InputStream;
-import java.io.InputStreamReader;
-
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.text.DecimalFormat;
-import java.text.NumberFormat;
-import java.text.ParseException;
 
 public class RESTClient {
-
-    Client client=Client.create();
-    WebResource webResource;
-    ClientResponse response;
     private static final Logger log = Logger.getLogger(RESTClient.class);
-    Number number = null;
-    static NumberFormat formatter = new DecimalFormat("#0.0000");
 
-    public double sendAndGetResult(String requestUrl) {
-        URL url = null;
-        try {
-            url = new URL(requestUrl);
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        }
+    public String getSendResponseAndResult(String URI, String Path, double firstNumber, double secondNumber) {
+        ClientConfig config = new DefaultClientConfig();
+        Client client = Client.create(config);
+        WebResource service = client.resource(URI);
+        log.info("Cant return response");
+        return getOutputAsText(service.path(Path).path(firstNumber + "/" + secondNumber));
+    }
 
-        HttpURLConnection request = null;
-        try {
-            request = (HttpURLConnection) url.openConnection();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        try {
-            request.connect();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        JsonParser jsonParser = new JsonParser();
-        JsonElement root = null;
-
-        try {
-            root = jsonParser.parse(new InputStreamReader((InputStream) request.getContent()));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        JsonObject rootobj = root.getAsJsonObject();
-        webResource = client.resource(requestUrl);
-        response = webResource.accept(MediaType.APPLICATION_JSON).get(ClientResponse.class);
-        return Double.valueOf(response.getEntity(String.class));
-
+    private static String getOutputAsText(WebResource service) {
+        return service.accept(MediaType.TEXT_PLAIN).get(String.class);
     }
 }
--- src/test/java/com/epam/RestClientTest.java
+++ src/test/java/com/epam/RestClientTest.java
@@ -1,9 +1,6 @@
 package com.epam;
 
 import com.epam.restclient.RESTClient;
-import com.sun.jersey.api.client.Client;
-import com.sun.jersey.api.client.ClientResponse;
-import com.sun.jersey.api.client.WebResource;
 import org.apache.log4j.Logger;
 import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
@@ -13,42 +10,49 @@ public class RestClientTest {
     private static final Logger LOG = Logger.getLogger(RestClientTest.class);
     private String restBaseUrl;
     RESTClient restClient;
+    private String ADD_PATH;
+    private String SUB_PATH;
+    private String DIVIDE_PATH;
+    private String MULT_PATH;
+    private String PERCENT_PATH;
 
     @BeforeTest
     public void setUp() {
-        restBaseUrl = "http://localhost:8080/epam/services/calc";
+        restBaseUrl = "http://localhost:8080/epam/services";
         restClient = new RESTClient();
+        ADD_PATH = "calc/add";
+        SUB_PATH = "calc/subtract";
+        DIVIDE_PATH = "calc/dividing";
+        MULT_PATH = "calc/multiply";
+        PERCENT_PATH = "calc/percent";
     }
 
     @Test
-    public void restAddTest() {
-        double firstNumber = 1;
-        double secondNumber = 2;
-        double expectedResult = 3.0000;
-        double result = restClient.sendAndGetResult(restBaseUrl + "/add/" + firstNumber + "/" + secondNumber);
-
+    public void restSubTest() {
+        double firstNumber = 3;
+        double secondNumber = 1;
+        String expectedResult = "2,0000";
+        String result = restClient.getSendResponseAndResult(restBaseUrl, SUB_PATH, firstNumber, secondNumber);
         LOG.info("Expected = " + expectedResult + " and actual = " + result);
         Assert.assertEquals(expectedResult, result);
     }
 
     @Test
-    public void restSubtractTest() {
-        double firstNumber = 5;
+    public void restAddTest() {
+        double firstNumber = 2;
         double secondNumber = 1;
-        double expectedResult = 4.0000;
-        double result = restClient.sendAndGetResult(restBaseUrl + "/subtract/" + firstNumber + "/" + secondNumber);
-
+        String expectedResult = "3,0000";
+        String result = restClient.getSendResponseAndResult(restBaseUrl, ADD_PATH, firstNumber, secondNumber);
         LOG.info("Expected = " + expectedResult + " and actual = " + result);
         Assert.assertEquals(expectedResult, result);
     }
 
     @Test
     public void restDivideTest() {
-        double firstNumber = 6;
-        double secondNumber = 3;
-        double expectedResult = 2.0000;
-        double result = restClient.sendAndGetResult(restBaseUrl + "/divide/" + firstNumber + "/" + secondNumber);
-
+        double firstNumber = 8;
+        double secondNumber = 2;
+        String expectedResult = "4,0000";
+        String result = restClient.getSendResponseAndResult(restBaseUrl, DIVIDE_PATH, firstNumber, secondNumber);
         LOG.info("Expected = " + expectedResult + " and actual = " + result);
         Assert.assertEquals(expectedResult, result);
     }
@@ -57,21 +61,19 @@ public class RestClientTest {
     public void restMultiplyTest() {
         double firstNumber = 5;
         double secondNumber = 2;
-        double expected = 10.0000;
-        double result = restClient.sendAndGetResult(restBaseUrl + "/multiply/" + firstNumber + "/" + secondNumber);
-
-        LOG.info("Expected = " + expected + " and actual = " + result);
-        Assert.assertEquals(expected, result);
+        String expectedResult = "10,0000";
+        String result = restClient.getSendResponseAndResult(restBaseUrl, MULT_PATH, firstNumber, secondNumber);
+        LOG.info("Expected = " + expectedResult + " and actual = " + result);
+        Assert.assertEquals(expectedResult, result);
     }
 
     @Test
     public void restPercentTest() {
-        double firstN = 13;
-        double secondN = 5;
-        double expected = 3.0000;
-        double result = restClient.sendAndGetResult(restBaseUrl + "/percent/" + firstN + "/" + secondN);
-
-        LOG.info("Expected = " + expected + " and actual = " + result);
-        Assert.assertEquals(expected, result);
+        double firstNumber = 13;
+        double secondNumber = 5;
+        String expectedResult = "3,0000";
+        String result = restClient.getSendResponseAndResult(restBaseUrl, PERCENT_PATH, firstNumber, secondNumber);
+        LOG.info("Expected = " + expectedResult + " and actual = " + result);
+        Assert.assertEquals(expectedResult, result);
     }
 }
